c911a7202daa513d85ff7c4a0efbe27798a9ea0d
Antonius Riha <antoniusriha@gmail.com>
2012-09-30 23:58:18 +0200

    [xbuild] Add NormalizePath task and adjust other tasks accordingly

--- build/GetRelPath.cs
+++ build/GetRelPath.cs
@@ -45,9 +45,7 @@ namespace Tasque.Build
 		public override bool Execute ()
 		{
 			try {
-				var fromPath = Path.GetFullPath (FromPath);
-				var toPath = Path.GetFullPath (ToPath);
-				RelativePath = MakeRelPath (fromPath, toPath);
+				RelativePath = InternalGetRelPath (FromPath, ToPath);
 			} catch (Exception ex) {
 				Log.LogErrorFromException (ex, true);
 				return false;
@@ -55,42 +53,35 @@ namespace Tasque.Build
 			return true;
 		}
 
-        string MakeRelPath (string fromPath, string toPath)
+        internal static string InternalGetRelPath (string fromPath, string toPath)
 		{
-			if (!Path.IsPathRooted (fromPath) || !Path.IsPathRooted (toPath))
-				throw new Exception ("All paths must be absolute.");
+			if (fromPath == null)
+				throw new ArgumentNullException ("fromPath");
+			if (toPath == null)
+				throw new ArgumentNullException ("toPath");
 
 			// normalize
-			fromPath = Path.GetFullPath (fromPath).TrimEnd (Path.DirectorySeparatorChar);
-			toPath = Path.GetFullPath (toPath).TrimEnd (Path.DirectorySeparatorChar);
-			
-			var fromPieces = fromPath.Split (Path.DirectorySeparatorChar);
-			var toPieces = toPath.Split (Path.DirectorySeparatorChar);
+			fromPath = NormalizePath.InternalNormalizePath (Path.GetFullPath (fromPath));
+			toPath = NormalizePath.InternalNormalizePath (Path.GetFullPath (toPath));
 
-			var fromBranch = new StringBuilder ();
-			var toBranch = new StringBuilder ();
-			var root = new StringBuilder ();
+			var basePathPieces = fromPath.Split (Path.DirectorySeparatorChar);
+			var targetPathPieces = toPath.Split (Path.DirectorySeparatorChar);
 
-			var longerPieces = fromPieces.Length > toPieces.Length ? fromPieces : toPieces;
+			var max = Math.Min (basePathPieces.Length, targetPathPieces.Length);
+			int i = 0;
+			for (; i < max; i++)
+				if (basePathPieces [i] != targetPathPieces [i])
+					break;
 
-			var isRootFinished = false;
-			for (int i = 0; i < longerPieces.Length; i++) {
-				if ((i < fromPieces.Length && i < toPieces.Length) && !isRootFinished
-				    && fromPieces [i] == toPieces [i])
-					root.Append (longerPieces [i] + Path.DirectorySeparatorChar);
-				else {
-					isRootFinished = true;
+			StringBuilder fromBranch = new StringBuilder (), toBranch = new StringBuilder ();
+			for (int j = i; j < basePathPieces.Length; j++)
+				fromBranch.Append (".." + Path.DirectorySeparatorChar.ToString ());
+			for (int j = i; j < targetPathPieces.Length; j++)
+				toBranch.Append (targetPathPieces [j] + Path.DirectorySeparatorChar.ToString ());
 
-					if (i < fromPieces.Length)
-						fromBranch.Append (".." + Path.DirectorySeparatorChar.ToString ());
-
-					if (i < toPieces.Length)
-						toBranch.Append (toPieces [i] + (i == toPieces.Length - 1 ? ""
-							: Path.DirectorySeparatorChar.ToString ()));
-				}
-			}
-
-			return fromBranch.ToString () + toBranch.ToString ();
+			var toBranchString = toBranch.ToString ();
+			var result = fromBranch.Append (toBranchString).ToString ();
+			return result.TrimEnd (Path.DirectorySeparatorChar);
         }
 	}
 }
