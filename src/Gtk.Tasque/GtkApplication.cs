afbf0b74a0cc47157efc59dabb7a27f850be2102
Antonius Riha <antoniusriha@gmail.com>
2012-10-26 19:02:49 +0200

    Use a .config file for path settings (DataDir)
    This enables better integration with WiX Windows Installer,
    because the value of DataDir can now be set during install
    time instead of compile time.

--- src/Gtk.Tasque/GtkApplication.cs
+++ src/Gtk.Tasque/GtkApplication.cs
@@ -53,9 +53,6 @@ namespace Tasque
 
 			// add package icon path to default icon theme search paths
 			Gtk.IconTheme.Default.PrependSearchPath (Defines.IconsDir);
-#if WIN
-			Gtk.IconTheme.Default.PrependSearchPath (Defines.GlobalIconsDir);
-#endif
 		}
 
 		public virtual void InitializeIdle ()
