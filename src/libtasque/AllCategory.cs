821a2edc0017ada4b0bbe5508e199bd67a3be8b8
Antonius Riha <antoniusriha@gmail.com>
2013-01-03 17:51:24 +0100

    Remove superfluous Preferences ref from AllCategory

--- src/libtasque/AllCategory.cs
+++ src/libtasque/AllCategory.cs
@@ -14,14 +14,11 @@ namespace Tasque
 		// from all categories will be shown.  Otherwise, only tasks from the
 		// specified lists will be shown.
 		List<string> categoriesToHide;
-
-		Preferences preferences;
 		
 		public AllCategory (Preferences preferences)
 		{
 			if (preferences == null)
 				throw new ArgumentNullException ("preferences");
-			this.preferences = preferences;
 			categoriesToHide =
 				preferences.GetStringList (Preferences.HideInAllCategory);
 			categoriesToHide = preferences.GetStringList (Preferences.HideInAllCategory);
